6fd33a9329534967a54011dbb585c47a68ee2296
Angela Graves <algraves78@gmail.com>
2017-07-21 20:22:01 -0400

    created the skeleton for the post router

---
+++ routers/post.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/posts', (req, res) =>{
+  res.send('getting all dem posts!!');
+});
+router.get('/posts/:postId', (req, res) =>{
+  res.send('getting that one special post!');
+});
+router.post('/posts', (req, res) =>{
+  res.send('creating that new post you requested');
+});
+router.put('/posts/:postId', (req, res) =>{
+  res.send('updates errrrywhere!');
+});
+router.delete('/posts/:postId', (req, res) =>{
+  res.send('burning it down!');
+});
+
+module.exports = router;
--- routers/user.router.js
+++ routers/user.router.js
@@ -7,13 +7,13 @@ router.get('/users', (req, res) =>{
 router.get('/users/:userId', (req, res) =>{
   res.send('getting that one special someone!');
 });
-router.post('/user', (req, res) =>{
+router.post('/users', (req, res) =>{
   res.send('creating that new user you requested');
 });
-router.put('/user/:userId', (req, res) =>{
+router.put('/users/:userId', (req, res) =>{
   res.send('updates errrrywhere!');
 });
-router.delete('/user/:userId', (req, res) =>{
+router.delete('/users/:userId', (req, res) =>{
   res.send('burning it down!');
 });
 
